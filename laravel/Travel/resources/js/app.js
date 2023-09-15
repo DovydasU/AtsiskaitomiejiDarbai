6d6fa9d8b731c110a8b04cf33342b4ccac75a7a6
DovydasU <ulonasd@gmail.com>
2023-09-15 15:13:25 +0300

    viskas istrinta nes nepavyko tik istrinti breeze

--- laravel/Travel/resources/js/app.js
+++
@@ -1,9 +0,0 @@
-import '/resources/scss/styles.scss'    
-import * as bootstrap from 'bootstrap';
-import './bootstrap';
-
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
