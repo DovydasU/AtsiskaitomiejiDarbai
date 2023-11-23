9a23286c2adc72d67ec4dee986a556ccedce6135
DovydasU <ulonasd@gmail.com>
2023-11-23 21:44:34 +0200

    padarytas user dropdown headeryje (netobulas)

--- laravel/travel/resources/views/layouts/base.blade.php
+++ laravel/travel/resources/views/layouts/base.blade.php
@@ -4,20 +4,40 @@
 <head>
     <title>WoOx - @yield('title')</title>
 
-    {{-- @vite(['resources/scss/style.scss']) --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
     <!-- Bootstrap core CSS -->
-    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}">
     <link rel="stylesheet" href="{{ asset('css/templatemo-woox-travel.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.css') }}">
     <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
-    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <style>
+        .custom-dropdown {
+            background-color: #22b3c1;
+        }
+
+        .custom-dropdown .dropdown-item {
+            color: white;
+            border-radius: 0.25rem;
+        }
+
+        .custom-dropdown .dropdown-item:hover {
+            background-color: #22b3c1;
+            color: white;
+        }
+    </style>
+
+    <!-- Include Jetstream styles -->
+    @livewireStyles
+    @stack('styles')
+
     @yield('styles')
 </head>
 
@@ -46,6 +66,15 @@
     <script src="{{ asset('js/tabs.js') }}"></script>
     <script src="{{ asset('js/popup.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Include Jetstream scripts -->
+    @livewireScripts
+    @stack('scripts')
+
     @yield('scripts')
 </body>
 
--- laravel/travel/resources/views/layouts/header.blade.php
+++ laravel/travel/resources/views/layouts/header.blade.php
@@ -3,37 +3,31 @@
         <div class="row">
             <div class="col-12">
                 <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
                     <a href="{{ route('index')}}" class="logo">
                         <img src="images/logo.png" alt="">
                     </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
                     <ul class="nav">
                         <li><a href="{{ route('index')}}" class="{{ request()->routeIs('index') ? 'active' : '' }}">Home</a></li>
                         <li><a href="{{ route('about')}}" class="{{ request()->routeIs('about') ? 'active' : '' }}">About</a></li>
                         <li><a href="{{ route('deals')}}" class="{{ request()->routeIs('deals') ? 'active' : '' }}">Deals</a></li>
                         <li><a href="{{ route('reservation')}}" class="{{ request()->routeIs('reservation') ? 'active' : '' }}">Reservation</a></li>
-                        <li>@if (Route::has('login'))
-                          <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                              @auth
-                                  <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                              @else
-                                  <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-          
-                                  @if (Route::has('register'))
-                                      <li><a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a></li>
-                                  @endif
-                              @endauth
-                          </div>
-                      @endif</li>
-                    </ul>   
-                    {{-- <a class='menu-trigger'>
-                        <span>Menu</span>
-                    </a> --}}
-                    <!-- ***** Menu End ***** -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <ul class="dropdown-menu custom-dropdown" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="{{ route('profile.show') }}">Profile</a></li>
+                                <li>
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <button type="submit" class="dropdown-item">Logout</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </nav>
             </div>
         </div>
     </div>
-  </header>
+</header>
